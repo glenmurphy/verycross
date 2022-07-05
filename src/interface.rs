8062c898ac03e02ac7efdb72c999f645d3f63127
Glen Murphy <glen@glenmurphy.com>
2022-07-05 14:09:08 +1000

    nicer names

--- src/interface.rs
+++ src/interface.rs
@@ -34,13 +34,13 @@ impl Interface {
         }
     }
 
-    fn show(&mut self) {
+    fn show_cross(&mut self) {
         self.event_proxy.send_event(InterfaceMessage::ShowCross).unwrap();
         self.tray.on();
         self.showing = true;
     }
 
-    fn hide(&mut self) {
+    fn hide_cross(&mut self) {
         self.event_proxy.send_event(InterfaceMessage::HideCross).unwrap();
         self.tray.off();
         self.showing = false;
@@ -49,8 +49,14 @@ impl Interface {
     fn set_cross(&mut self, x: usize) {
         self.event_proxy.send_event(InterfaceMessage::SetCross(x)).unwrap();
     }
+    
+    fn toggle_cross(&mut self) {
+        if self.showing { self.hide_cross() } else { self.show_cross() }
+    }
 
-    fn open_config(&mut self) {
+    fn toggle_config(&mut self) {
+        // Toggling doesn't really make UI sense, but we have it here so we can test
+        // our ability to communicate with the interior of the fltk window.
         if self.config_open {
             self.config.close();
             self.config_open = false;
@@ -58,10 +64,6 @@ impl Interface {
             self.config.open();
             self.config_open = true;
         }
-    }
-
-    fn toggle(&mut self) {
-        if self.showing { self.hide() } else { self.show() }
     }
 
     fn quit(&mut self) {
@@ -75,22 +77,22 @@ impl Interface {
             tokio::select! {
                 Some(key_event) = self.key_rx.recv() => {
                     match key_event {
-                        (Key::ScrollLock, true) => self.toggle(),
+                        (Key::ScrollLock, true) => self.toggle_cross(),
                         _ => {}
                     }
                 },
                 Some(msg) = self.tray.recv() => {
                     match msg {
-                        tray::TrayMessage::Show => self.show(),
-                        tray::TrayMessage::Hide => self.hide(),
-                        tray::TrayMessage::Config => self.open_config(),
+                        tray::TrayMessage::Show => self.show_cross(),
+                        tray::TrayMessage::Hide => self.hide_cross(),
+                        tray::TrayMessage::Config => self.toggle_config(),
                         tray::TrayMessage::Quit => self.quit(),
                     }
                 },
                 Some(msg) = self.config.recv() => {
                     match msg {
-                        config::ConfigMessage::ShowCross => self.show(),
-                        config::ConfigMessage::HideCross => self.hide(),
+                        config::ConfigMessage::ShowCross => self.show_cross(),
+                        config::ConfigMessage::HideCross => self.hide_cross(),
                         config::ConfigMessage::SetCross(x) => self.set_cross(x),
                         config::ConfigMessage::ConfigClosed => self.config_open = false,
                     }
