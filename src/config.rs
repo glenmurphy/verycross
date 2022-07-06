6317a2e69c390bec1205f22f917b8a942ff3e0e4
Glen Murphy <glen@glenmurphy.com>
2022-07-06 12:00:41 +1000

    naming/comments

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,7 @@ use tokio::sync::mpsc::{unbounded_channel, UnboundedReceiver, UnboundedSender};
 pub struct ConfigInterface {
     open_tx: UnboundedSender<()>,
     config_rx: UnboundedReceiver<ConfigMessage>,
-    control_tx: app::Sender<Control>,
+    control_tx: app::Sender<Event>,
 }
 
 #[allow(unused)]
@@ -16,11 +16,11 @@ impl ConfigInterface {
 
     pub fn close(&mut self) {
         println!("Sending close");
-        self.control_tx.send(Control::Close);
+        self.control_tx.send(Event::Close);
     }
 
     pub fn settings_changed(&mut self) {
-        self.control_tx.send(Control::SettingsChanged);
+        self.control_tx.send(Event::SettingsChanged);
     }
 
     pub async fn recv(&mut self) -> Option<ConfigMessage> {
@@ -29,7 +29,7 @@ impl ConfigInterface {
 }
 
 #[derive(Copy, Clone, Debug)]
-enum Control {
+enum Event {
     // Stuff from inside the app
     ShowButton,
     HideButton,
@@ -50,15 +50,15 @@ pub enum ConfigMessage {
     ConfigClosed,
 }
 
-fn create_crosshair_buttons(control_tx: app::Sender<Control>) -> Vec<Button> {
+fn create_crosshair_buttons(control_tx: app::Sender<Event>) -> Vec<Button> {
     let mut c0 = Button::new(50, 50, 80, 30, "Cross 0");
-    c0.emit(control_tx, Control::SetCrossButton(0));
+    c0.emit(control_tx, Event::SetCrossButton(0));
 
     let mut c1 = Button::new(50, 90, 80, 30, "Cross 1");
-    c1.emit(control_tx, Control::SetCrossButton(1));
+    c1.emit(control_tx, Event::SetCrossButton(1));
 
     let mut c2 = Button::new(50, 130, 80, 30, "Cross 2");
-    c2.emit(control_tx, Control::SetCrossButton(2));
+    c2.emit(control_tx, Event::SetCrossButton(2));
 
     vec![c0, c1, c2]
 }
@@ -74,8 +74,8 @@ fn update_crosshair_buttons(buttons: &mut Vec<Button>, index: usize) {
 /// other than its own.
 fn app_loop(
     config_tx: UnboundedSender<ConfigMessage>,
-    control_tx: app::Sender<Control>,
-    control_rx: app::Receiver<Control>,
+    event_tx: app::Sender<Event>,
+    event_rx: app::Receiver<Event>,
     open_rx: &mut UnboundedReceiver<()>,
 ) {
     loop {
@@ -87,29 +87,29 @@ fn app_loop(
         let mut frame = Frame::new(0, 0, 400, 200, "");
         frame.set_label("hello");
 
-        let mut buttons = create_crosshair_buttons(control_tx.clone());
+        let mut buttons = create_crosshair_buttons(event_tx.clone());
         update_crosshair_buttons(&mut buttons, settings::get().crosshair);
 
-        Button::new(50, 210, 60, 30, "Hide").emit(control_tx, Control::HideButton);
-        Button::new(150, 210, 60, 30, "Show").emit(control_tx, Control::ShowButton);
-        Button::new(250, 210, 60, 30, "Close").emit(control_tx, Control::CloseButton);
+        Button::new(50, 210, 60, 30, "Hide").emit(event_tx, Event::HideButton);
+        Button::new(150, 210, 60, 30, "Show").emit(event_tx, Event::ShowButton);
+        Button::new(250, 210, 60, 30, "Close").emit(event_tx, Event::CloseButton);
 
         wind.end();
         wind.show();
 
         while app.wait() {
-            match control_rx.recv() {
+            match event_rx.recv() {
                 // Stuff from the app
-                Some(Control::ShowButton) => config_tx.send(ConfigMessage::ShowCross).unwrap(),
-                Some(Control::HideButton) => config_tx.send(ConfigMessage::HideCross).unwrap(),
-                Some(Control::SetCrossButton(n)) => {
+                Some(Event::ShowButton) => config_tx.send(ConfigMessage::ShowCross).unwrap(),
+                Some(Event::HideButton) => config_tx.send(ConfigMessage::HideCross).unwrap(),
+                Some(Event::SetCrossButton(n)) => {
                     config_tx.send(ConfigMessage::SetCross(n)).unwrap()
                 }
-                Some(Control::CloseButton) => break,
+                Some(Event::CloseButton) => break,
 
                 // Stuff from outside the app
-                Some(Control::Close) => break,
-                Some(Control::SettingsChanged) => {
+                Some(Event::Close) => break,
+                Some(Event::SettingsChanged) => {
                     update_crosshair_buttons(&mut buttons, settings::get().crosshair);
                     app.redraw();
                 }
@@ -123,23 +123,24 @@ fn app_loop(
 }
 
 pub fn new() -> ConfigInterface {
-    // Used within the app, and by the control_loop to feed events into the app
-    let (control_tx, control_rx) = fltk::app::channel::<Control>();
+    // The app's main event handling mechanism; used internally by buttons,
+    // but also to feed content into the app
+    let (event_tx, event_rx) = fltk::app::channel::<Event>();
 
-    // Used by the app to send messages to the control_loop
+    // Used by the app to send messages outwards
     let (config_tx, config_rx) = unbounded_channel::<ConfigMessage>();
 
-    // Used by control_loop to signal the app to open
+    // Used to signal the app to open
     let (open_tx, mut open_rx) = unbounded_channel::<()>();
 
-    // We use a non-async thread so we're not blocking sends on the config_tx channels
-    std::thread::spawn(move || {
-        app_loop(config_tx, control_tx, control_rx, &mut open_rx);
+    // The app is not async and blocks on its own event loop
+    tokio::task::spawn_blocking(move || {
+        app_loop(config_tx, event_tx, event_rx, &mut open_rx);
     });
 
     ConfigInterface {
         open_tx,
         config_rx,
-        control_tx,
+        control_tx: event_tx,
     }
 }
